a4e69f2dbab2b6c899bf7bd0116ac8a6ed28fc29
huangyifan <huangyifan@4399inc.com>
2020-03-05 17:44:32 +0800

    no message

--- buildSrc/src/main/groovy/com/hyif/plugin/CustomPlugin.groovy
+++ buildSrc/src/main/groovy/com/hyif/plugin/CustomPlugin.groovy
@@ -7,7 +7,6 @@ import org.gradle.api.Project
 class CustomPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        System.out.println(666666666666666666666666666)
         AppExtension appExtension = project.extensions.getByType(AppExtension.class);
         appExtension.registerTransform(new CustomTransform(project))
     }
--- buildSrc/src/main/groovy/com/hyif/plugin/CustomTransform.groovy
+++ buildSrc/src/main/groovy/com/hyif/plugin/CustomTransform.groovy
@@ -9,7 +9,7 @@ import org.gradle.api.Project
 class CustomTransform extends Transform {
     Project project
 
-    public CustomTransform(Project project) {
+    CustomTransform(Project project) {
         this.project = project
     }
 
@@ -33,25 +33,30 @@ class CustomTransform extends Transform {
         return false
     }
 
+
     @Override
-    void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
-        // Transform的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
-        inputs.each { TransformInput input ->
+    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        def inputs = transformInvocation.inputs
+        def outputProvider = transformInvocation.outputProvider
+        inputs.each { TransformInput transformInput ->
+            // Transform的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
             //对类型为“文件夹”的input进行遍历
-            input.directoryInputs.each { DirectoryInput directoryInput ->
+            transformInput.directoryInputs.each { DirectoryInput directoryInput ->
                 //文件夹里面包含的是我们手写的类以及R.class、BuildConfig.class以及R$XXX.class等
-                MyInject.injectDir(directoryInput.file.absolutePath, "com\\hyif\\hcplugin")
+//                MyInject.injectDir(directoryInput.file.absolutePath, "com\\example\\use_plugin")
+                System.out.println("directoryInput.name="+directoryInput.name)
                 // 获取output目录
                 def dest = outputProvider.getContentLocation(directoryInput.name,
                         directoryInput.contentTypes, directoryInput.scopes,
                         Format.DIRECTORY)
+
+                // 将input的目录复制到output指定目录
                 FileUtils.copyDirectory(directoryInput.file, dest)
             }
             //对类型为jar文件的input进行遍历
-            input.jarInputs.each { JarInput jarInput ->
-
+            transformInput.jarInputs.each { JarInput jarInput ->
                 //jar文件一般是第三方依赖库jar文件
-
+                System.out.println("jarInput.name="+jarInput.name)
                 // 重命名输出文件（同目录copyFile会冲突）
                 def jarName = jarInput.name
                 def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
@@ -65,7 +70,5 @@ class CustomTransform extends Transform {
                 FileUtils.copyFile(jarInput.file, dest)
             }
         }
-
-
     }
 }
